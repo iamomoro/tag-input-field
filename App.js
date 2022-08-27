2616effc3b3812a11a14d76096c8b73737cc4646
Elvis Omoro <iamomoroit@gmail.com>
2022-08-27 14:44:32 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TagInput from './taginput/TagInput';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Enter Some Tags ...</h2>
+      <TagInput />
+    </div>
+  );
+}
+
+export default App;
